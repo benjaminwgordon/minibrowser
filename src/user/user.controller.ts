c504e7144886c2b9055debca87dcd8c6128c8c5c
Benjamin Gordon <ben@Benjamins-MacBook-Pro.local>
2022-08-25 16:57:21 -0500

    user creation and deletion

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,4 +12,14 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('/signup')
+  async signup(@Body() userData: { username: string; email: string }) {
+    return this.userService.createUser(userData);
+  }
+
+  @Post('delete')
+  async delete(@Body() userData: { username: string }) {
+    return this.userService.deleteUser(userData);
+  }
 }
