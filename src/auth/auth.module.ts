acb8c60e929ea3fbefcb4d776a55c74f8bc27ddb
Benjamin Gordon <ben@Benjamins-MacBook-Pro.local>
2022-08-25 17:30:28 -0500

    add password hash to user schema

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService]
+  imports: [],
+  providers: [AuthService, UserService, PrismaService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,23 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from '../user/user.service';
-import { AuthDTO } from '../../dist/auth/dto/auth.dto';
-import * as argon from 'argon2'
+import { AuthDTO } from './dto/auth.dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService){}
+  constructor(
+    private prisma: PrismaService,
+    private UserService: UserService,
+  ) {}
 
-    async signup(dto: AuthDTO){
-        const hash = await 
-    }
+  async signup(dto: AuthDTO) {
+    const hash = await argon.hash(dto.password);
+    const user = await this.UserService.createUser({
+      email: dto.email,
+      username: dto.username,
+      hash: hash,
+    });
+    return user;
+  }
 }
