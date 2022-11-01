4800d2b85fd43efeed04f2fc15d32de790b9db91
Benjamin Gordon <ben@Benjamins-MacBook-Pro.local>
2022-11-01 14:46:57 -0500

    remove api key

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,13 +5,11 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(config: ConfigService) {
-    console.log({
-      db_status: `Dialing database at: postgresql://minibrowser-db:saucy-emu@localhost/cloudsql/rosy-petal-365623:us-central1:minibrowser-db/.s.PGSQL.5432`,
-    });
+    console.log(process.env.DB_CONNECTION_STRING);
     super({
       datasources: {
         db: {
-          url: 'postgresql://minibrowser-db:saucy-emu@localhost/postgres?host=/cloudsql/rosy-petal-365623:us-central1:minibrowser-db',
+          url: process.env.DB_CONNECTION_STRING,
         },
       },
     });
